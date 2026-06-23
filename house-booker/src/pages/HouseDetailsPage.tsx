80ce019ed1f9a90a4ba0dfa0a05e0f21f9dde464
orestisLyk <orestis.lyk@outlook.com>
2026-06-23 17:59:22 +0300

    feat: add delete and view bookings options for owned houses

--- house-booker/src/pages/HouseDetailsPage.tsx
+++ house-booker/src/pages/HouseDetailsPage.tsx
@@ -1,9 +1,12 @@
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import { getHouseById } from "../api/housesApi";
 import type { HouseReadOnly } from "../shared/types/house";
 import { useEffect, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
 
 const HouseDetailsPage = () => {
+
+    const { user, isAuthenticated } = useAuth();
 
     const id = useParams().id;
     const [houseReadOnly, setHouseReadOnly] = useState<HouseReadOnly | null>(null);
@@ -19,15 +22,21 @@ const HouseDetailsPage = () => {
 
     return (
         <>
-            <h1 className="text-3xl font-bold mb-4">{houseReadOnly?.name}</h1>
-            <div className="mb-4">
+            <h1 className="text-3xl font-bold mb-4 mx-4">{houseReadOnly?.name}</h1>
+            <div className="mb-4 mx-4">
                 {houseReadOnly?.imageUrls && houseReadOnly.imageUrls.length > 0 && houseReadOnly.imageUrls.map((url, index) => (
-                    <img key={index} src={url} alt={`${houseReadOnly.name} ${index + 1}`} className="w-full h-64 object-cover rounded mb-4" />
+                    <img key={index} src={url} alt={`${houseReadOnly.name} ${index + 1}`} className="w-lg h-auto object-cover rounded mb-4" />
                 ))}
             </div>
-            <p className="text-gray-600 mb-2">{houseReadOnly?.description}</p>
-            <p className="text-gray-600 mb-2">{houseReadOnly?.address}, {houseReadOnly?.region}</p>
-            <p className="text-gray-800 font-semibold">${houseReadOnly?.pricePerNight} per night</p>
+            <p className="text-gray-600 mb-2 mx-4">{houseReadOnly?.description}</p>
+            <p className="text-gray-600 mb-2 mx-4">{houseReadOnly?.address}, {houseReadOnly?.region}</p>
+            <p className="text-gray-800 font-semibold mx-4">${houseReadOnly?.pricePerNight} per night</p>
+            {isAuthenticated && user?.role === "Owner" && user.ownerId === houseReadOnly?.ownerId && (
+                <>
+                    <button className=" mx-4 bg-primary text-white px-4 py-2 rounded hover:bg-primary-dark mt-4">Delete</button>
+                    <Link to={"/bookings/by-house/" + houseReadOnly?.id} className="mx-4 *:bg-primary text-white px-4 py-2 rounded hover:bg-primary-dark mt-4 ml-2">View Bookings</Link>
+                </>
+            )}
         </>
     )
 }
