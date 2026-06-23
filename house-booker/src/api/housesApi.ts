c001d153d2ccf59b86635aff4d038d281e8e641f
orestisLyk <orestis.lyk@outlook.com>
2026-06-23 20:09:08 +0300

    feat: add createHouse function

--- house-booker/src/api/housesApi.ts
+++ house-booker/src/api/housesApi.ts
@@ -1,4 +1,4 @@
-import type { HouseReadOnly } from "../shared/types/house";
+import type { HouseCreate, HouseReadOnly } from "../shared/types/house";
 import type { PaginatedObject } from "../shared/types/core";
 import api from "./axios";
 
@@ -15,5 +15,15 @@ export const getHouses = async (page: number=1, size: number=10): Promise<Pagina
 
 export const getHouseById = async (id: string): Promise<HouseReadOnly> => {
     const response = await api.get<HouseReadOnly>(`/houses/${id}`);
+    return response.data;
+}
+
+export const getHousesByOwnerId = async (ownerId: number) : Promise<HouseReadOnly[]> => {
+    const response = await api.get<HouseReadOnly[]>(`/houses/by-owner/${ownerId}`);
+    return response.data;
+}
+
+export const createHouse = async (house: HouseCreate) : Promise<HouseReadOnly> => {
+    const response = await api.post<HouseReadOnly>("/houses", house);
     return response.data;
 }
