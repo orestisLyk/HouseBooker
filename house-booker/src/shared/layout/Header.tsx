5c9e415788ebd29789c9cf3bc2b0694da101a6cd
orestisLyk <orestis.lyk@outlook.com>
2026-06-23 17:48:57 +0300

    feat: add reactive logic to authentication and role

--- house-booker/src/shared/layout/Header.tsx
+++ house-booker/src/shared/layout/Header.tsx
@@ -1,6 +1,17 @@
 import { Link } from "react-router";
+import { useAuth } from "../../hooks/useAuth";
 
 const Header = () => {
+    const  { isAuthenticated, user, logout } = useAuth();
+
+    let isOwner = null;
+    let isRenter = null;
+    let isAdmin = null;
+    if(isAuthenticated) {
+        isOwner = user?.role.includes("Owner");
+        isRenter = user?.role.includes("Renter");
+        isAdmin = user?.role.includes("Admin");
+    }
 
     return (
         <>
@@ -12,12 +23,44 @@ const Header = () => {
                     <Link to="/" className="text-white p-4">
                         Home
                     </Link>
-                    <Link to="/login" className="text-white p-4">
-                        Login
-                    </Link>
-                    <Link to="/register" className="text-white p-4">
-                        Register
-                    </Link>
+                    {isRenter && (
+                        <>
+                            <span className="text-white p-4">Welcome, {user?.username}</span>
+                            <button onClick={logout} className="text-white p-4">Logout</button>
+                        </>
+                    )}
+                    {isOwner && (
+                        <>
+                            <span className="text-white p-4">Welcome, {user?.username}</span>
+                            <Link to={`/houses/by-owner/${user?.ownerId}`} className="text-white p-4">
+                                My Houses
+                            </Link>
+                            <Link to="/houses/new" className="text-white p-4">
+                                Add House
+                            </Link>
+                            <button onClick={logout} className="text-white p-4">Logout</button>
+                        </>
+                    )}
+                    {isAdmin && (
+                        <>
+                            <span className="text-white p-4">Welcome, {user?.username}</span>
+                            <Link to="/admin" className="text-white p-4">
+                                Admin Panel
+                            </Link>
+                            <button onClick={logout} className="text-white p-4">Logout</button>
+                        </>
+                    )}
+
+                    {!isAuthenticated && (
+                        <>
+                            <Link to="/login" className="text-white p-4">
+                                Login
+                            </Link>
+                            <Link to="/register" className="text-white p-4">
+                                Register
+                            </Link>
+                        </>
+                    )}
                 </div>
             </div>
         </>
