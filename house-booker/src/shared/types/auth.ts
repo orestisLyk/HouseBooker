fc8fae87603e1a5c90d8d1e3fb18f227e32262e6
orestisLyk <orestis.lyk@outlook.com>
2026-06-23 15:54:05 +0300

    chore: add types

--- house-booker/src/shared/types/auth.ts
+++ house-booker/src/shared/types/auth.ts
@@ -16,11 +16,37 @@ export type RegisterRequest = {
     roleId: number;
 }
 
-export type JwtPayload = {
-    sub: string;
+export interface AuthUser {
+    id: number;
     username: string;
-    role: string;
     email: string;
-    renterId?: number;
+    role: string;
     ownerId?: number;
+    renterId?: number;
 }
+
+export interface JwtPayload {
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": string;
+
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name": string;
+
+    "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress": string;
+
+    "http://schemas.microsoft.com/ws/2008/06/identity/claims/role": string;
+
+    OwnerId?: string;
+    RenterId?: string;
+
+    exp: number;
+    iss: string;
+    aud: string;
+}
+
+export interface AuthContextType {
+    user: AuthUser | null;
+    token: string | null;
+    isAuthenticated: boolean;
+    login: (token: string) => void;
+    logout: () => void;
+}
+
