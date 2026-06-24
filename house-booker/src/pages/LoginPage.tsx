8e123c963e6eb32ca1849a1248e3471231f60255
orestisLyk <orestis.lyk@outlook.com>
2026-06-24 12:55:05 +0300

    feat: refactor to add validation with react hook form and zod

--- house-booker/src/pages/LoginPage.tsx
+++ house-booker/src/pages/LoginPage.tsx
@@ -1,46 +1,48 @@
-import type { LoginRequest, LoginResponse } from "../shared/types/auth";
+import { LoginSchema, type LoginRequest, type LoginResponse } from "../shared/types/auth";
 import { loginApi } from "../api/auth";
 import { useNavigate } from "react-router";
 import { useAuth } from "../hooks/useAuth";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const LoginPage = () => {
     const navigate = useNavigate();
     const { login } = useAuth();
 
-    
+    const {
+        register,
+        handleSubmit,
+        formState: { errors , isSubmitting },
+    } = useForm<LoginRequest>({
+        resolver: zodResolver(LoginSchema)      
+    });
 
-    const handleSubmit = async (event: React.SubmitEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const username = formData.get("username") as string;    
-        const password = formData.get("password") as string;
-
-        const credentials : LoginRequest = {
-            username,
-            password
-        };
-
-        const token : LoginResponse = await loginApi(credentials);
-
-        login(token.token);
-
-        navigate("/", { replace: true });
-    }
+    const onSubmit = async (data: LoginRequest) => {
+        try {
+            const response: LoginResponse = await loginApi(data);
+            login(response.token);
+            navigate("/", { replace: true });
+        } catch (error) {
+            console.error("Login failed:", error);
+        }
+    };
 
 
     return (
         <>
             <h1 className="text-3xl font-bold mb-4">Login</h1>
-            <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
+            <form className="max-w-md mx-auto" onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
                     <label htmlFor="username" className="block text-gray-700 font-bold mb-2">Username</label>
-                    <input type="text" id="username" name="username" className="w-full p-2 border rounded" />
+                    <input type="text" id="username" {...register("username")} className="w-full p-2 border rounded" />
+                    {errors.username && <p className="text-red-500 text-sm mt-1">{errors.username.message}</p>}
                 </div>
                 <div className="mb-4">
                     <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
-                    <input type="password" id="password" name="password" className="w-full p-2 border rounded" />
+                    <input type="password" id="password" {...register("password")} className="w-full p-2 border rounded" />
+                    {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>} 
                 </div>
-                <button type="submit" className="bg-primary text-white px-4 py-2 rounded hover:bg-primary-dark">Login</button>
+                <button disabled={isSubmitting} type="submit" className="bg-primary text-white px-4 py-2 rounded hover:bg-primary-dark">Login</button>
             </form>
         </>
     )
--- house-booker/src/pages/RegisterPage.tsx
+++ house-booker/src/pages/RegisterPage.tsx
@@ -1,38 +1,24 @@
 import { useNavigate } from "react-router";
 import { registerApi } from "../api/auth";
-import type { RegisterRequest } from "../shared/types/auth";
+import { RegisterSchema, type RegisterRequest } from "../shared/types/auth";
 import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod/src/index.js";
+import { useForm } from "react-hook-form";
 
 const RegisterPage = () => {
     const navigate = useNavigate();
 
-    const handleSubmit = async (event: React.SubmitEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const formData = new FormData(event.currentTarget);
-        const username = formData.get("username") as string;
-        const email = formData.get("email") as string;
-        const firstName = formData.get("firstname") as string;
-        const lastName = formData.get("lastname") as string;
-        const password = formData.get("password") as string;
-        const roleId = parseInt(formData.get("roleId") as string);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<RegisterRequest>({
+        resolver: zodResolver(RegisterSchema),
+    });
 
-        const registerRequest: RegisterRequest = {
-            username,
-            email,
-            firstName,
-            lastName,
-            password,
-            roleId
-        };
-
+    const onSubmit = async (data: RegisterRequest) => {
         try {
-            const response: boolean = await registerApi(registerRequest);
-
-            if (!response) {
-                alert("Registration failed. Please try again.");
-                return;
-            }
-
+            await registerApi(data);
             navigate("/login", { replace: true });
         } catch (error) {
             if (axios.isAxiosError(error)) {
@@ -48,44 +34,50 @@ const RegisterPage = () => {
                 alert("An unexpected error occurred. Please try again later.");
             }
         }
-    }
+    };
 
     return (
         <>
             <h1 className="text-3xl font-bold mb-4">Register</h1>
-            <form className="max-w-md mx-auto bg-white p-8 rounded shadow-md" onSubmit={handleSubmit}>
+            <form className="max-w-md mx-auto bg-white p-8 rounded shadow-md" onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
                     <label htmlFor="username" className="block text-gray-700 font-bold mb-2">Username</label>
-                    <input type="text" id="username" name="username" className="w-full p-2 border rounded" />
+                    <input type="text" id="username" {...register("username")} className="w-full p-2 border rounded" />
+                    {errors.username && <p className="text-red-500 text-sm mt-1">{errors.username.message}</p>}
                 </div>
                 <div className="mb-4">
                     <label htmlFor="email" className="block text-gray-700 font-bold mb-2">Email</label>
-                    <input type="email" id="email" name="email" className="w-full p-2 border rounded" />
+                    <input type="email" id="email" {...register("email")} className="w-full p-2 border rounded" />
+                    {errors.email && <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>}
                 </div>
 
                 <div className="mb-4">
                     <label htmlFor="firstname" className="block text-gray-700 font-bold mb-2">First Name</label>
-                    <input type="text" id="firstname" name="firstname" className="w-full p-2 border rounded" />
+                    <input type="text" id="firstname" {...register("firstName")} className="w-full p-2 border rounded" />
+                    {errors.firstName && <p className="text-red-500 text-sm mt-1">{errors.firstName.message}</p>}
                 </div>
 
                 <div className="mb-4">
                     <label htmlFor="lastname" className="block text-gray-700 font-bold mb-2">Last Name</label>
-                    <input type="text" id="lastname" name="lastname" className="w-full p-2 border rounded" />
+                    <input type="text" id="lastname" {...register("lastName")} className="w-full p-2 border rounded" />
+                    {errors.lastName && <p className="text-red-500 text-sm mt-1">{errors.lastName.message}</p>}
                 </div>
 
                 <div className="mb-4">
                     <label htmlFor="roleId" className="block text-gray-700 font-bold mb-2">Role</label>
-                    <select id="roleId" name="roleId" className="w-full p-2 border rounded">
-                        <option value="2">Owner</option>
-                        <option value="3">Renter</option>
+                    <select id="roleId" {...register("roleId")} className="w-full p-2 border rounded">
+                        <option value={2}>Owner</option>
+                        <option value={3}>Renter</option>
                     </select>
-                                    </div>
+                    {errors.roleId && <p className="text-red-500 text-sm mt-1">{errors.roleId.message}</p>}
+                </div>
 
                 <div className="mb-4">
                     <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
-                    <input type="password" id="password" name="password" className="w-full p-2 border rounded" />
+                    <input type="password" id="password" {...register("password")} className="w-full p-2 border rounded" />
+                    {errors.password && <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>}
                 </div>
-                <button type="submit" className="bg-primary text-white px-4 py-2 rounded hover:bg-primary-dark">Register</button>
+                <button disabled={isSubmitting} type="submit" className="bg-primary text-white px-4 py-2 rounded hover:bg-primary-dark">Register</button>
             </form>
         </>
     )
